0f5953f8651fb47743fbfe07fa544e339f4cb55d
Abdullah Bilgin <abilgin02@icloud.com>
2022-08-06 10:42:02 +0300

    Day006: How to use a for loop to repeat work

---
+++ Day_006/How to use a for loop to repeat work .playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+for os in platforms {
+    print("Swift works great on \(os)")
+}
+
+// loop body
+// a loop iteration
+// os loop variable
+
+for name in platforms {
+    print("Swift works great on \(name)")
+}
+
+for rubberChicken in platforms {
+    print("Swift works great on \(rubberChicken)")
+}
+
+for a in 1...12 {
+    print("5 x \(a) is \(5 * a)")
+}
+
+// nested loop
+
+for i in 1...12 {
+    print("The \(i) times table")
+    
+    for j in 1...12 {
+        print(" \(j) x \(i) is \(j * i)")
+    }
+}
+
+for i in 1...5 {
+    print("Counting from 1 through 5: \(i)")
+}
+print()
+
+for i in 1..<5 {
+    print("Counting 1 up to 5: \(i)")
+}
+
+// 1...5 as “one through five”,
+// 1..<5 as “one up to five,”
+
+var lyric = "Haters gonna"
+
+for _ in 1...5 {
+    lyric += " hate"
+    
+   print(lyric)
+}
+ 
+print(lyric)
