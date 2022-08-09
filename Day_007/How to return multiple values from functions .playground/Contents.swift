29ecef97521e63e99050bece15a736e778cad1bf
Abdullah Bilgin <abilgin02@icloud.com>
2022-08-09 06:55:43 +0300

    Day:008 -> restudy : When is the return keyword not needed in a Swift function -> now understood:)

--- Day_007/How to return multiple values from functions .playground/Contents.swift
+++ Day_007/How to return multiple values from functions .playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func isUppercase(string: String) -> Bool {
+    string == string.uppercased()
+}
+
+isUppercase(string: "Abdullah")
+isUppercase(string: "ABDULLAH")
+//
+//func getUser() -> [String] {
+//    ["Taylor", "Swift"]
+//}
+//let user = get = getUser()
+//print("Name: \(user[0] \(user[1]))")
+//
+
+//
+//func getUser() -> [String: String] {
+//    [
+//        "firstName"  : "Taylor",
+//        "lastName": "Swwift"
+//    ]
+//}
+//
+//let user = getUser()
+//
+//print("Name \(user["firstName", default: "Anonmous"]) \(user["lastName",default: "Anonmous"])")
+
+func getUser() -> (firstName: String, lastName: String) {
+    (firstName: "Taylor", lastName: "Swift")
+}
+
+let user = getUser()
+print("Name: \(user.firstName) \(user.lastName)")
--- Day_007/How to return values from functions .playground/Contents.swift
+++ Day_007/How to return values from functions .playground/Contents.swift
@@ -1,49 +1,69 @@
 import UIKit
 
-let root = sqrt(49)
-print(root)
+let root = sqrt(169)
 
-func rollDice() -> Int {
+//....
+
+func rollDice() -> Int{
     return Int.random(in: 1...6)
-    
 }
 
 let result = rollDice()
 print(result)
 
-//func compare(one: String, second: String) {
-//    one.sorted()
-//    second.sorted()
-//    if one == second{
-//        print("\(one) is \(second)")
-//    } else{
-//            print("\(one) is not \(second)")
-//    }
-//
-//}
-//
-//compare(one: "abc", second: "cbc")
-//
+//...
 
-//func areLattersIdentical(string1: String, string2: String) -> Bool{
-//    let first = string1.sorted()
-//    let second = string2.sorted()
-//    return first == second
-//}
-//
-//areLattersIdentical(string1: "abc", string2: "cba")
+// variable for test
+var string1: String = "abc"
+var string2: String = "cba"
 
-func areLattersIdentical(string1: String, string2: String) -> Bool {
-    return string1.sorted() == string2.sorted()
+// sorted() func
+string1.sorted()
+string2.sorted()
+
+// compare by if
+
+if string1.sorted() == string2.sorted() {
+    print("\(string1) includes same latter as \(string2)")
+} else {
+    print("\(string1) does not include same latter as \(string2)")
 }
 
 
-areLattersIdentical(string1: "abc", string2: "cba")
+// using func to get result
+func areLettersIdentical1(string1: String, string2: String) -> Bool {
+    let first = string1.sorted()
+    let second = string2.sorted()
+    return first == second
+}
+
+areLettersIdentical1(string1: string1, string2: string2)
+
+
+// less code
+
+func areLetterIdentical2(string1: String, string2: String) -> Bool {
+    return string1.sorted() == string2.sorted()
+}
+
+areLetterIdentical2(string1: string1, string2: string2)
+
+// if one line no need return keyword
+
+func areLettersIdentical3(string1: String, string2: String) -> Bool {
+    string1.sorted() == string2.sorted()
+}
+
+areLettersIdentical3(string1: string1, string2: string2)
+
+// ...
 
 func rollDice2() -> Int {
     Int.random(in: 1...6)
 }
 rollDice2()
+
+// Pythagorean theorem
 
 func pythagoras(a: Double, b: Double) -> Double {
     let input = a * a + b * b
@@ -54,67 +74,42 @@ func pythagoras(a: Double, b: Double) -> Double {
 let c = pythagoras(a: 3, b: 4)
 print(c)
 
+// boiled down to a single line
+
 func pythagoras2(a: Double, b: Double) -> Double {
     sqrt(a * a + b * b)
 }
-let c2 = pythagoras2(a: 3, b: 5)
-print(c)
 
-let isAdmin = true
-let isOwner = false
-let isEditingEnable = false
-
-isOwner == true && isEditingEnable || isAdmin == true
-
-let name = "Otis"
-
-if name == "Maeve" {
-    print("Hello, Maeve")
-}
-
-func doMath() -> Int {
-    return 5 + 5
-}
-
-func doMoreMath() -> Int {
-    5 + 5
-}
-doMath()
-doMoreMath()
-
+print(pythagoras2(a: 3, b: 4))
 
 func greet(name: String) -> String {
     if name == "Taylor Swift" {
-        return "oh Wow!"
+        return "Oh wow!"
     } else {
         return "Hello, \(name)"
     }
 }
 
-greet(name: "Taylor Swift")
+print(greet(name: "Abdullah"))
+print(greet(name: "Taylor Swift"))
 
-func greet2(name2: String) -> String {
-    if name2 == "Taylor Swift" {
-        return "oh Wow!"
-    } else {
-      return "Hello, \(name2)" // if  missing return
-    }
+
+//If we wanted to remove the return statements in there, we could not write this:
+//
+//func greet(name: String) -> String {
+//    if name == "Taylor Swift" {
+//        "Oh wow!"
+//    } else {
+//        "Hello, \(name)"
+//    }
+//}
+
+func greet2(name: String) -> String {
+    "Oh wow!"
 }
+greet2(name: "abdullah")
 
-greet2(name2: "Taylor Swift")
-
-func greet3(name3: String) -> String {
-    name3 == "Taylor Swift" ? "Oh wow!" : "Hello, \(name3)"
-}
-
-greet3(name3: "Abdulla")
-
-func greet4(name4:String) -> String {
-    "Oh, wow!"
-}
-
-greet4(name4: "Abdullah")
-
+// Question 1/12: This code is valid Swift – true or false?
 
 func read(books: [String]) -> Bool {
     for book in books {
@@ -123,7 +118,18 @@ func read(books: [String]) -> Bool {
     return true
 }
 
-read(books: ["a","b"])
+read(books: ["Book-1", "Book-2"])
+
+func read2(books: [String]) {
+    for book in books {
+        print("I'm reading \(book)")
+    }
+    return
+}
+
+read2(books: ["Book-1", "Book-3"])
+
+// Question 2/12: This code is valid Swift – true or false?
 
 func writeToLog(message: String) -> Bool {
     if message != "" {
@@ -133,10 +139,13 @@ func writeToLog(message: String) -> Bool {
         return false
     }
 }
-writeToLog(message: "")
-writeToLog(message: "Hello Welcome")
 
-func check(scores: [Int]) -> Bool {
+writeToLog(message: "")
+writeToLog(message: "Error 404")
+
+//Question 3/12: This code is valid Swift – true or false?
+
+func check(scores: [Int]) -> Bool{ // there is no Bool
     for score in scores {
         if score < 80 {
             return false
@@ -144,34 +153,40 @@ func check(scores: [Int]) -> Bool {
     }
     return true
 }
-
 check(scores: [100, 90, 100, 85])
 
 
-func burnCandles(count: Int) -> Int {
+// Question 4/12: This code is valid Swift – true or false?
+
+func burnCandles(count: Int) -> Bool { // it return bool not an integer
     for _ in 1...count {
         print("I'm lighting a candle")
     }
-    return count
+    return true
 }
 
-burnCandles(count: 12)
+burnCandles(count: 5)
+
+// Question 5/12: This code is valid Swift – true or false?
 
 func paintHouse(color: String) -> Bool {
     if color == "tartan" {
-        
+        return true // not here
     }
     return false
 }
-
 paintHouse(color: "tartan")
+paintHouse(color: "green")
 
+// Question 6/12: This code is valid Swift – true or false?
 
 func format(number: Int) -> String {
     return "The number is \(number)"
 }
 
 format(number: 12)
+
+// Question 7/12: This code is valid Swift – true or false?
 
 func countMultiplesOf10(numbers: [Int]) -> Int {
     var result = 0
@@ -184,34 +199,37 @@ func countMultiplesOf10(numbers: [Int]) -> Int {
 }
 countMultiplesOf10(numbers: [5, 10, 15, 20, 25])
 
+// Question 8/12: This code is valid Swift – true or false?
+
 func giveDog(food: String) -> String {
     if food == "treat" {
-        print("The dog ate it")
+        return   "The dog ate it"
     } else {
-        "The dog stared in disgust"
+        return  "The dog stared in disgust"
     }
-    
-    return food
-    
 }
 
 giveDog(food: "treat")
+giveDog(food: "fish")
 
+// Question 9/12: This code is valid Swift – true or false?
 
-func estimateCost(units: Int) -> Int {
+func estimateCost(units: Int) -> String { // return String
     switch units {
     case 0...10:
-        return units * 10
+        return "\(units * 10)"
     case 11...50:
-        return units * 9
+        return "\(units * 9)"
     case 51...100:
-        return units * 8
+        return "\(units * 8)"
     default:
-        return 0
+        return "We can't make that many."
     }
 }
 
-print(estimateCost(units: -10))
+estimateCost(units: 25)
+
+// Question 10/12: This code is valid Swift – true or false?
 
 func allTestsPassed(tests: [Bool]) -> Bool {
     for test in tests {
@@ -221,13 +239,19 @@ func allTestsPassed(tests: [Bool]) -> Bool {
     }
     return true
 }
-print(allTestsPassed(tests: [true, true]))
+
+allTestsPassed(tests: [false])
+
+// Question 11/12: This code is valid Swift – true or false?
 
 func playPiano(song: String) -> String {
-    return "I'm going to play \(song) on my piano."
+    return "I'm going to play \(song) on my piano." // retrurn -> return spelling mistake
 }
 
-playPiano(song: "hello")
+playPiano(song: "Hello")
+
+
+// Question 12/12: This code is valid Swift – true or false?
 
 func isEveryoneAdult(ages: [Int]) -> Bool {
     for age in ages {
@@ -237,5 +261,7 @@ func isEveryoneAdult(ages: [Int]) -> Bool {
     }
     return true
 }
-isEveryoneAdult(ages: [10, 20, 16, 24])
+var A = isEveryoneAdult(ages: [10, 20, 16, 24])
+isEveryoneAdult(ages: [20, 20, 19, 24])
 
+print(A)
